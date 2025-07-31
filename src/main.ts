0bed5af7c9ba534a7184073154c5513caa770667
Matthew Fournier <matthew@halfhelix.com>
2025-07-31 16:34:42 -0400

    no message

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { Octokit, App } from 'octokit'
+import * as github from '@actions/github'
+import { Octokit } from 'octokit'
 
 /**
  * The main function for the action.
@@ -9,21 +10,31 @@ export async function run(): Promise<void> {
   try {
     const githubToken = process.env.GITHUB_TOKEN
     const octokit = new Octokit({ auth: githubToken })
-    const {
-      data: { login }
-    } = await octokit.rest.users.getAuthenticated()
-    console.log('Hello, %s', login)
 
     const directories = core.getInput('directories', {
       required: true,
       trimWhitespace: true
     })
-    const prNumberString = core.getInput('pr-number', {
-      required: true,
-      trimWhitespace: true
+    const prNumber = Number(
+      core.getInput('pr-number', {
+        required: true,
+        trimWhitespace: true
+      })
+    )
+
+    const { owner, repo } = github.context.repo
+
+    const files = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: prNumber
     })
 
-    const prNumber = prNumberString ? Number(prNumberString) : null
+    console.log(`Checking directories: ${directories}`)
+    console.log(
+      `Changed files:`,
+      files.data.map((f) => f.filename)
+    )
 
     console.log(githubToken, prNumber, directories)
 
