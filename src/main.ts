14ddeef80acaabaf63491d3382c3a1a471a4f922
Matthew Fournier <matthew@halfhelix.com>
2025-07-31 15:42:36 -0400

    no message

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,25 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
 
 /**
  * The main function for the action.
- *
- * @returns Resolves when the action is complete.
+ * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const input: string = core.getInput('input', {
+      required: true,
+      trimWhitespace: true
+    })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const githubToken = core.getInput('github_token', { required: true })
+    const prNumber = parseInt(core.getInput('pr_number', { required: true }))
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    console.log(githubToken, prNumber)
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('branchThemeId')
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
+
+run()
